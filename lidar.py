8bb7a4e3bbd40200089fc23e2e1a089fa43fa737
Ravi3191 <55124454+Ravi3191@users.noreply.github.com>
2020-03-30 01:39:41 -0400

    Update lidar.py

--- lidar.py
+++ lidar.py
@@ -6,8 +6,15 @@ Created on Sun Mar 29 22:15:15 2020
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 29 22:15:15 2020
 
+@author: ravit
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
 proj_H = 64
 proj_W = 2048
 proj_fov_up = 3
@@ -19,7 +26,8 @@ remissions = np.zeros((0, 1), dtype=np.float32)    # [m ,1]: remission
   
 proj_pgm = np.full((proj_H, proj_W, 5), -1,
                           dtype=np.float32)
-
+proj_pgm1 = np.full((proj_H, proj_W, 5), -1,
+                          dtype=np.float32)
 
 # for each point, where it is in the range image
 proj_x = np.zeros((0, 1), dtype=np.float32)        # [m, 1]: x
@@ -69,8 +77,21 @@ scan_z = scan_z[required_range]
 
 depth = depth[required_range]
 remissions = remissions[required_range]
+"""
+# scale to image size using angular resolution
+proj_x *= proj_W                              # in [0.0, W]
+proj_y *= proj_H                              # in [0.0, H]
 
+# round and clamp for use as index
+proj_x = np.floor(proj_x)
+proj_x = np.minimum(proj_W - 1, proj_x)
+proj_x = np.maximum(0, proj_x).astype(np.int32)   # in [0,W-1]
+proj_x_copy = np.copy(proj_x)  # store a copy in orig order
 
+proj_y = np.floor(proj_y)
+proj_y = np.minimum(proj_H - 1, proj_y)
+proj_y = np.maximum(0, proj_y).astype(np.int32)   # in [0,H-1]
+proj_y_copy = np.copy(proj_y)  # stope a copy in original order"""
 
 # scale to image size using angular resolution
 proj_x *= proj_W                              # in [0.0, W]
@@ -87,17 +108,21 @@ proj_y = np.minimum(proj_H - 1, proj_y)
 proj_y = np.maximum(0, proj_y).astype(np.int32)   # in [0,H-1]
 proj_y_copy = np.copy(proj_y)  # stope a copy in original order
 
-
+"""
 # assing to images
 #polar_grid_map = np.concatenate((points,remissions,depth),axis = 2)
+proj_pgm[proj_y,proj_x,0:3]  = points
+proj_pgm[proj_y,proj_x,3]  = remissions
+proj_pgm[proj_y,proj_x,4]  = depth"""
 
 proj_pgm[proj_y,proj_x,0:3]  = points
 proj_pgm[proj_y,proj_x,3]  = remissions
 proj_pgm[proj_y,proj_x,4]  = depth
 
-proj_pgm[:,767:1279,:] = proj_pgm[:,767:1279,:]
-"""
+proj_pgm = proj_pgm[:,767:1279,:]
+
 plt.imshow(proj_pgm[:,:,1])
 plt.show()
-plt.imshow(proj_pgm1[:,767:1279,4])
+"""
+plt.imshow(proj_pgm1[:,:,4])
 plt.show()"""
