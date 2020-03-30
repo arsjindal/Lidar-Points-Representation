3b36a43b005ffb5ecc89f394f4337814b8a38e43
Ravi3191 <55124454+Ravi3191@users.noreply.github.com>
2020-03-30 00:06:46 -0400

    Update lidar.py

--- lidar.py
+++ lidar.py
@@ -1,1 +1,132 @@
 # This is for creating architetures which deal with point clouds
+class LaserScan:
+  """Class that contains LaserScan with x,y,z,r"""
+  EXTENSIONS_SCAN = ['.bin']
+
+  def __init__(self, project=False, H=64, W=512, fov_up=3.0, fov_down=-25.0):
+    self.project = project
+    self.proj_H = H
+    self.proj_W = W
+    self.proj_fov_up = fov_up
+    self.proj_fov_down = fov_down
+    self.reset()
+
+  def reset(self):
+    """ Reset scan members. """
+    self.points = np.zeros((0, 3), dtype=np.float32)        # [m, 3]: x, y, z
+    self.remissions = np.zeros((0, 1), dtype=np.float32)    # [m ,1]: remission
+
+    self.proj_pgm = np.full((self.proj_H, self.proj_W, 5), -1,
+                            dtype=np.float32)
+
+    # for each point, where it is in the range image
+    self.proj_x = np.zeros((0, 1), dtype=np.float32)        # [m, 1]: x
+    self.proj_y = np.zeros((0, 1), dtype=np.float32)        # [m, 1]: y
+
+  def size(self):
+    """ Return the size of the point cloud. """
+    return self.points.shape[0]
+
+  def __len__(self):
+    return self.size()
+
+  def open_scan(self, filename):
+    """ Open raw scan and fill in attributes
+    """
+    # reset just in case there was an open structure
+    self.reset()
+
+    # check filename is string
+    if not isinstance(filename, str):
+      raise TypeError("Filename should be string type, "
+                      "but was {type}".format(type=str(type(filename))))
+
+    # check extension is a laserscan
+    if not any(filename.endswith(ext) for ext in self.EXTENSIONS_SCAN):
+      raise RuntimeError("Filename extension is not valid scan file.")
+
+    # if all goes well, open pointcloud
+    scan = np.fromfile(filename, dtype=np.float32)
+    scan = scan.reshape((-1, 4))
+
+    # put in attribute
+    points = scan[:, 0:3]    # get xyz
+    remissions = scan[:, 3]  # get remission
+    self.set_points(points, remissions)
+
+  def set_points(self, points, remissions=None):
+    """ Set scan attributes (instead of opening from file)
+    """
+    # reset just in case there was an open structure
+    self.reset()
+
+    # check scan makes sense
+    if not isinstance(points, np.ndarray):
+      raise TypeError("Scan should be numpy array")
+
+    # check remission makes sense
+    if remissions is not None and not isinstance(remissions, np.ndarray):
+      raise TypeError("Remissions should be numpy array")
+
+    # put in attribute
+    self.points = points    # get xyz
+    if remissions is not None:
+      self.remissions = remissions  # get remission
+    else:
+      self.remissions = np.zeros((points.shape[0]), dtype=np.float32)
+
+    # if projection is wanted, then do it and fill in the structure
+    if self.project:
+      self.do_range_projection()
+
+  def do_range_projection(self):
+    """ Project a pointcloud into a spherical projection image.projection.
+        Function takes no arguments because it can be also called externally
+        if the value of the constructor was not set (in case you change your
+        mind about wanting the projection)
+    """
+    # laser parameters
+    fov_up = self.proj_fov_up / 180.0 * np.pi      # field of view up in rad
+    fov_down = self.proj_fov_down / 180.0 * np.pi  # field of view down in rad
+    fov = abs(fov_down) + abs(fov_up)  # get field of view total in rad
+
+    # get depth of all points
+    depth = np.linalg.norm(self.points, 2, axis=1)
+
+    # get scan components
+    scan_x = self.points[:, 0]
+    scan_y = self.points[:, 1]
+    scan_z = self.points[:, 2]
+
+    # get angles of all points
+    yaw = -np.arctan2(scan_y, scan_x)
+    pitch = np.arcsin(scan_z / depth)
+
+    # get projections in image coords
+    proj_x = 0.5 * (yaw / np.pi + 1.0)          # in [0.0, 1.0]
+    proj_y = 1.0 - (pitch + abs(fov_down)) / fov        # in [0.0, 1.0]
+    
+    required_range = np.logical_and(yaw>=-np.pi/4, yaw <= np.pi/4)
+    
+    proj_x = proj_x(required_range)
+    proj_y = proj_y(required_range)
+
+    # scale to image size using angular resolution
+    proj_x *= self.proj_W                              # in [0.0, W]
+    proj_y *= self.proj_H                              # in [0.0, H]
+
+    # round and clamp for use as index
+    proj_x = np.floor(proj_x)
+    proj_x = np.minimum(self.proj_W - 1, proj_x)
+    proj_x = np.maximum(0, proj_x).astype(np.int32)   # in [0,W-1]
+    self.proj_x = np.copy(proj_x)  # store a copy in orig order
+
+    proj_y = np.floor(proj_y)
+    proj_y = np.minimum(self.proj_H - 1, proj_y)
+    proj_y = np.maximum(0, proj_y).astype(np.int32)   # in [0,H-1]
+    self.proj_y = np.copy(proj_y)  # stope a copy in original order
+
+
+    # assing to images
+    polar_grid_map = np.vstack((points,remission,depth))
+    self.proj_pgm[proj_y,proj_x]  = polar_grid_map
