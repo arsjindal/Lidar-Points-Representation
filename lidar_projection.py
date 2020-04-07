88285140159e7b1a93a30513e55d632e2e197bf5
rodri651 <rodri651@seas.upenn.edu>
2020-04-06 21:18:10 -0400

    cross checking labels on projected lidar and rgb

--- lidar_projection.py
+++ lidar_projection.py
@@ -76,7 +76,7 @@ def project_lidar2img_plane(scan,pixel_coor,rgb,label,valid_scans):
 	proj_pgm[proj_y,proj_x,0:3]  = points
 	proj_pgm[proj_y,proj_x,3]  = remissions
 	proj_pgm[proj_y,proj_x,4]  = depth
-    
+	
 	proj_pgm[proj_y[valid_scans],proj_x[valid_scans],6:]  = rgb[(pixel_coor[1, :]).astype(int),\
 										(pixel_coor[0, :]).astype(int)]/255.0
 	proj_pgm[proj_y,proj_x,5]  = label
@@ -140,6 +140,21 @@ def find_correspondance(scan,proj_cam2lidar,rgb,label):
 	
 	pixel_coor = pts_2d[:, valid_scans]
 	
+	
+
+	colors = [[255,255,255],[255,0,0],[0,255,0],[0,0,255]]
+	valid_labels = label[valid_scans]
+	label_image = np.zeros(rgb.shape,dtype=np.uint8)
+	for i in range(pixel_coor.shape[1]):
+		color = colors[int(valid_labels[i])]
+		cv2.circle(label_image,(pixel_coor[0,i].astype(int), pixel_coor[1,i].astype(int)),2,color=tuple(color),thickness=-1)
+	blend = cv2.addWeighted(rgb, 0.7, label_image, 0.3, 1)
+
+	plt.imshow(blend)
+	
+
+
+
 	vel_coor = scan#[valid_scans]
 
 	label = label#[valid_scans]
@@ -173,17 +188,17 @@ def load_label(path):
     
 def main():
     
-    rgb = cv2.cvtColor(cv2.imread(os.path.join('data/seq_data/5/000000.png')), cv2.COLOR_BGR2RGB)
-    label = load_label('data/seq_data/5/000000.label')
-    calib = load_calib('data/seq_data/5/calib.txt')
-    scan = load_lidar('data/seq_data/5/000000.bin')
+    rgb = cv2.cvtColor(cv2.imread(os.path.join('data/seq_data/3/000000.png')), cv2.COLOR_BGR2RGB)
+    label = load_label('data/seq_data/3/000000.label')
+    calib = load_calib('data/seq_data/3/calib.txt')
+    scan = load_lidar('data/seq_data/3/000000.bin')
     
     proj_cam2lidar = cam_2_lidar(calib)
     pgm = find_correspondance(scan, proj_cam2lidar, rgb, label)
 
     
-    fig, (ax1,ax2,ax3,ax4,ax5,ax6,ax7) = plt.subplots(7,1)
-
+    fig, (ax1,ax2,ax3,ax4,ax5,ax6,ax7,ax8) = plt.subplots(8,1)
+    
     ax1.set_title('x')
     ax2.set_title('y')
     ax3.set_title('z')
@@ -191,6 +206,7 @@ def main():
     ax5.set_title('depth')
     ax6.set_title('label')
     ax7.set_title('rgb')
+    ax8.set_title('label_projected')
     ax1.imshow(pgm[...,0])
     ax2.imshow(pgm[...,1])
     ax3.imshow(pgm[...,2])
@@ -198,7 +214,11 @@ def main():
     ax5.imshow(pgm[...,4])
     ax6.imshow(pgm[...,5])
     ax7.imshow(pgm[...,6:])
-    #plt.tight_layout()
+
+    color = np.array([[0,0,0],[1,0,0],[0,1,0],[0,0,1]],dtype=np.float32)
+
+    blend = cv2.addWeighted(pgm[...,6:]*255, 0.6, color[pgm[...,5].astype(int)]*255, 0.4, 1)
+    ax8.imshow(blend.astype(int))
     plt.subplots_adjust(top=1)
     plt.show()
 
